ed1f07b27bd4dc71dd685c4931240fce2bad5eb0
Vitalii <86424813+VitalikMk@users.noreply.github.com>
2023-12-08 19:54:37 +0200

    blog

---
+++ blog/src/lib.rs
@@ -0,0 +1,20 @@
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
+    }
+}
+
+
+trait State{}
+
+struct Draft {}
+
+impl State for Draft {}
