972884997efae2a644b07d1c5abedf7a98e6cc32
Vitalii <86424813+VitalikMk@users.noreply.github.com>
2023-12-11 14:14:43 +0200

    reject

--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -1,3 +1,5 @@
+use std::io::SeekFrom;
+
 pub struct Post {
     state: Option<Box<dyn State>>,
     content: String,
@@ -16,7 +18,7 @@ impl Post {
     }
 
     pub fn content(&self) -> &str {
-        ""
+        self.state.as_ref().unwrap().content(self)
     }
 
     pub fn request_review(&mut self) {
@@ -24,20 +26,41 @@ impl Post {
             self.state = Some(s.request_review())
         }
     }
+    
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
+
+    pub fn reject(&mut self) -> &str {
+        self.state = self.state.reject();
+    }
 }
 
 
 trait State{
+    fn approve(self: Box<Self>) -> Box<dyn State>;
     fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+    fn reject(self: Box<Self>) -> Box<dyn State>;
 }
 
-struct Draft {
-
-}
+struct Draft {}
 
 impl State for Draft {
     fn request_review(self: Box<Self>) -> Box<dyn State> {
         Box::new(PendingReview {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
     }
 }
 
@@ -46,5 +69,45 @@ struct PendingReview {}
 impl State for PendingReview {
     fn request_review(self: Box<Self>) -> Box<dyn State> {
         self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        let x = Box::new(Draft);
+        x
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct Reject {}
+
+impl State for Reject {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn reject(self: Box<Self>) -> Box<dyn State> {
+        &post.
     }
 }
